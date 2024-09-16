10b2c9b43b63a3096a7067bbf6fbbe7a9363b577
Nagakburos <wansanice@gmail.com>
2024-09-16 09:03:53 -0300

    Beta Version 1.0.0

--- lib/config/app_config.dart
+++ lib/config/app_config.dart
@@ -3,8 +3,7 @@ class AppConfig {
       'https://sites.google.com/view/mausvpn/in%C3%ADcio';
   static const String aboutUsLink =
       'https://sites.google.com/view/mausvpn/in%C3%ADcio';
-  static const String bannerAdUnitId = 'ca-app-pub-9932971012663465~6017604934';//bannermausvpnandroid = ca-app-pub-9932971012663465/6795471731
+  static const String bannerAdUnitId = 'ca-app-pub-9932971012663465/6795471731';//test = ca-app-pub-9932971012663465~6017604934
   static const String interstitialAdUnitId =
-      'ca-app-pub-9932971012663465~6017604934';//Intersticialmausvpnandroid = ca-app-pub-9932971012663465/9661336473
+      'ca-app-pub-9932971012663465/9661336473';//test = ca-app-pub-9932971012663465~6017604934
 }
-//TODO mudar codigos para produçao
